05265956918620e0c9d366fe26602b985fad01fc
thallard <thallard@student.42lyon.fr>
2021-03-27 17:46:05 +0100

    FragTrap exercice done

--- day03/ex00/FragTrap.hpp
+++ day03/ex00/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 13:37:42 by thallard          #+#    #+#             */
-/*   Updated: 2021/03/25 13:51:26 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/03/27 17:26:07 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 #define FRAGTRAP_HPP
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <cstdlib>
 
 
 class FragTrap
 {
 private:
-	int hit_points;
-	int max_hit_points;
+	unsigned int hit_points;
+	unsigned int max_hit_points;
 	int energy_points;
-	int max_energy_points;
+	unsigned int max_energy_points;
 	int level;
 	std::string name;
 	int melee_attack_damage;
@@ -31,12 +33,14 @@ private:
 public:
 	FragTrap();
 	FragTrap(std::string Name);
+	FragTrap(FragTrap const & frag);
 	~FragTrap();
 	FragTrap &operator=(FragTrap const & ref);
 	void rangedAttack(std::string const & target);
 	void meleeAttack(std::string const & target);
 	void beRepaired(unsigned int amount);
 	void takeDamage(unsigned int amount);
+	void vaulthunter_dot_exe(std::string const & target);
 };
 
 
--- day03/ex00/main.cpp
+++ day03/ex00/main.cpp
@@ -6,8 +6,33 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 13:37:36 by thallard          #+#    #+#             */
-/*   Updated: 2021/03/25 13:37:40 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/03/27 17:25:18 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+
+int main()
+{
+	std::srand(std::time(nullptr));
+	FragTrap *frag1 = new FragTrap("Robert");
+
+	frag1->rangedAttack("Benjamin");
+	frag1->meleeAttack("Thomas");
+	std::cout << std::endl;
+	
+	frag1->takeDamage(30);
+	frag1->beRepaired(50);
+	std::cout << std::endl;
+
+	frag1->takeDamage(30000);
+	frag1->beRepaired(10);
+	std::cout << std::endl;
+
+	frag1->vaulthunter_dot_exe("Genji");
+	frag1->vaulthunter_dot_exe("Hanzo");
+	frag1->vaulthunter_dot_exe("Tracer");
+	frag1->vaulthunter_dot_exe("Mercy");
+	frag1->vaulthunter_dot_exe("Soldier");
+	delete frag1;
+}
