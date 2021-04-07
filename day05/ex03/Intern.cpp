fbc73cb0f5d19dab16a46b4f7a6f52150329392c
thallard <thallard@student.42lyon.fr>
2021-04-07 21:31:59 +0200

    Fixed day05, day06 bugs

--- day05/ex03/Intern.cpp
+++ day05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 13:02:15 by thallard          #+#    #+#             */
-/*   Updated: 2021/04/03 13:48:44 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/04/07 21:31:41 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,40 @@ Intern::~Intern()
 {
 }
 
+// std::string Intern::lst[3] = {
+// 	"shuberry creation",
+// 	"robotomy request",
+// 	"presidential pardon"
+// };
+// typedef Form * (Intern::*form_constr) (std::string target);
+// Form *Intern::makeForm(std::string type, std::string name) {
+// 	Form *res = NULL;
+// 	form_constr	constr [3] = {&Intern::execShrubbery, &Intern::execRobotomy, &Intern::execPresidential};
 
-Form &Intern::makeForm(std::string name, std::string target)
-{
+// 	for(int i = 0; i < 3; i++)
+// 		if (type == lst[i])
+// 			res = (this->*constr[i])(name);
+// 	if (res != NULL)
+// 		std::cout << "Intern creates " << *res << std::endl;
+// 	else
+// 		std::cout << "Error, unrecognized Form type !" << std::endl;
+// 	return (res);
+// }
 
-	names = name;
 
-}
+// Form &Intern::makeForm(std::string name, std::string target)
+// {
+
+// 	Form *res = NULL;
+// 	form_constr	constr [3] = {&Intern::execShrubbery, &Intern::execRobotomy, &Intern::execPresidential};
+
+// 	for(int i = 0; i < 3; i++)
+// 		if (type == lst[i])
+// 			res = (this->*constr[i])(name);
+// 	if (res != NULL)
+// 		std::cout << "Intern creates " << *res << std::endl;
+// 	else
+// 		std::cout << "Error, unrecognized Form type !" << std::endl;
+// 	return (res);
+
+// }
--- day06/ex01/serialisation.hpp
+++ day06/ex01/serialisation.hpp
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 15:26:09 by thallard          #+#    #+#             */
-/*   Updated: 2021/04/07 15:36:24 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2021/04/07 19:47:34 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 #define SERALISATION_HPP
 #include <string>
 #include <iostream>
-#include <cstdlib>
-// struct Data { std::string s1; int n; std::string s2; };
+#include <ctime>
+
+struct Data
+{
+	std::string s1;
+	int n;
+	std::string s2;
+};
+
 
 #endif
